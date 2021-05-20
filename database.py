dc6e76b157d61cf13aa0b73a1e4af28bbb5aab5c
Кирилл Галимзянов <totalboy@MacBook-Pro-Kirill.local>
2021-05-20 23:37:51 +0500

    Добавлены кнопки

--- database.py
+++ database.py
@@ -1,15 +1,44 @@
+import logging as log
 import sqlite3
 
-class Database:
-    def __init__(self, path) -> None:
-        self.path = path
-        self.db = sqlite3.connect(self.path)
-        self.cursor = self.db.cursor()
 
-    def is_user_in_db(self, login) -> bool:
-        rows = self.cursor.execute(f'SELECT * FROM users WHERE login={login}')
-        return len(rows) > 0
+class Database():
+    def __init__(self, db_path) -> None:
+        self.db_path = db_path
 
-    def close(self):
-        self.db.commit()
-        self.db.close()
+    def is_user_in_db(self, username):
+        with sqlite3.connect(self.db_path) as con:
+            cur = con.cursor()
+            cur.execute('SELECT COUNT(*) FROM users WHERE login=?',
+                        (username,))
+            data = cur.fetchall()
+            con.commit()
+            return data[0][0] != 0
+
+    def add_user(self, username):
+        with sqlite3.connect(self.db_path) as con:
+            cur = con.cursor()
+            cur.execute('INSERT INTO users (login) VALUES (?)', (username,))
+            con.commit()
+            log.info(f'Added new user({username})')
+
+    def get_user_interests(self, username):
+        with sqlite3.connect(self.db_path) as con:
+            cur = con.cursor()
+            cur.execute("""
+                SELECT id, interest_id FROM user_interests WHERE user_id=
+                    (SELECT users.id FROM users WHERE users.login=?)""", (username,))
+            data = cur.fetchall()
+            con.commit()
+            return data
+
+    def get_no_user_interests(self, username):
+        with sqlite3.connect(self.db_path) as con:
+            cur = con.cursor()
+            cur.execute("""
+                SELECT id, name FROM interests WHERE id NOT IN 
+                (SELECT interest_id FROM user_interests WHERE user_id=
+                (SELECT users.id FROM users WHERE users.login=?))""", (username,))
+            data = cur.fetchall()
+            con.commit()
+            return data
--- main.py
+++ main.py
@@ -1,25 +1,114 @@
-import sqlite3
 import telebot
 import sqlite3
+import sqlite3
+import logging as log
 
-from icecream import ic
+from os import device_encoding
+from sqlite3.dbapi2 import sqlite_version_info
+
+from telebot import types
+from telebot.apihelper import send_message
+from telebot.types import InlineKeyboardButton, InlineKeyboardMarkup, KeyboardButton
+
 from database import Database
 
+# Constants
+
 token = '1720790674:AAFVmWMGe3p3a3O5xX63AAsrAie0WqYl_1Y'
-db_path = 'data.db'
+db_path = '/Users/totalboy/Desktop/Diploma/data.db'
 
+# Main handles
+
+log.basicConfig(filename='logfile.log',
+                format='[%(levelname)s] : %(asctime)s > %(message)s', level=log.INFO)
+bot = telebot.TeleBot(token, parse_mode='Markdown')
 db = Database(db_path)
-bot = telebot.TeleBot(token, parse_mode=None)
 
-@bot.message_handler(func=lambda m: True)
+# Bot's functions
+
+
+@bot.message_handler(commands=['start'])
+def start_bot(message):
+    log.info(f'Start chatting with user({message.from_user.username})')
+    send_welcome(message.chat.id, message.from_user.username)
+
+    if not db.is_user_in_db(message.from_user.username):
+        db.add_user(message.from_user.username)
+
+
+@bot.message_handler(commands=['help'])
+def send_help(message):
+    bot.send_message(message.from_user.id, 'Я - бот, который помогает найти интересную информацию для вас на основе ваших предпочтений и интересов.\nЯ обладаю следующими командами:\n1. /start - Начать диалог\n2. /help - Помощь по боту\n3. /info - Выдать интересную информацию\n4. /reset - Перевыбрать интересы\n')
+
+
+@bot.message_handler(content_types=['text'])
+def process_buttons(message):
+    username = message.from_user.username
+    chat_id = message.chat.id
+    text = message.text.lower()
+
+    if text in 'помощь':
+        send_help(message)
+    elif text == 'узнать интересную информацию':
+        db.get_no_user_interests(username)
+    elif text == 'добавить интересы':
+        interests = db.get_no_user_interests(username)
+        select_interests(build_keybord_with_interests(interests), chat_id)
+    elif text == 'сбросить интересы':
+        pass
+    elif text == 'назад':
+        send_main_message(chat_id)
+    else:
+        send_error(message)
+
+# Additional functions for bot
+
+
+def send_main_message(chat_id):
+    keyboard = get_main_keyboard()
+    bot.send_message(
+        chat_id, 'Выберите одну из функций с помощью кнопок.', reply_markup=keyboard)
+
+
+def send_welcome(chat_id, username):
+    keyboard = get_main_keyboard()
+    bot.send_message(chat_id, f'*Здравствуйте, {username}!*\n\nЯ смотрю, вы первый раз здесь.\nДавайте я вам все расскажу!\n\nЯ - бот, который помогает найти интересную информацию для вас на основе ваших предпочтений и интересов.\n\nДля начала мне нужно узнать ваши интересы. Нажимайте кнопочки под чатом, чтобы выбрать интересующую вас категорию.\nПосле выбора всех интересующих вас категорий нажмите кнопку "Закончить выбор", чтобы я начал вам выдавать интересную информацию.\n\nТакже перечисляю ниже свои команды:\n1. /start - Начать диалог\n2. /help - Помощь по боту\n3. /info - Выдать интересную информацию\n4. /reset - Перевыбрать интересы\n', reply_markup=keyboard)
+
+
+def send_error(message):
+    bot.send_message(message.chat.id,
+                     'Я не знаю такую команду!\nВоспользуйтесь командой /help для того, чтобы узнать мои команды.\n')
+
+
+def select_interests(keyboard, chat_id):
+    bot.send_message(
+        chat_id, 'Выберите соответствущие категории, которые вам подходят, нажимая на кнопки.', reply_markup=keyboard)
+
+
+def get_main_keyboard():
+    keyboard = telebot.types.ReplyKeyboardMarkup(True)
+    main_list = ['Помощь', 'Узнать интересную информацию',
+                 'Добавить интересы', 'Сбросить интересы']
+    btns = [KeyboardButton(name) for name in main_list]
+    keyboard.add(*btns)
+    return keyboard
+
+
+def build_keybord_with_interests(interests):
+    keyboard = telebot.types.ReplyKeyboardMarkup(True)
+    btns = [KeyboardButton(item[1]) for item in interests]
+    btns.append(get_back_button())
+    keyboard.add(*btns)
+    return keyboard
+
+
+def get_back_button():
+    return KeyboardButton('Назад')
+
+
+@bot.message_handler(func=lambda message: True)
 def echo_all(message):
-    with sqlite3.connect(db_path) as con:
-        cur = con.cursor()
-        cur.execute(f'SELECT COUNT(*) FROM users WHERE login=?', (message.from_user.username,))
-        data = cur.fetchall()
+    bot.reply_to(message, message.text)
 
-        ans = 'Найден' if data[0][0] != 0 else 'Не найден!'
-        bot.send_message(message.from_user.id, ans)
 
 bot.polling()
-db.close()
